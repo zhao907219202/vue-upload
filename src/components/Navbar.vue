051e2a4a36837c350f7ba8405614a2ded0c5ff99
zhaoyu <xhzhaoyu1229@126.com>
2018-12-02 11:03:06 +0800

    vue-upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <span class="navbar-brand mb-0 h1">Vue-SpringBoot</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  </nav>
+</template>
